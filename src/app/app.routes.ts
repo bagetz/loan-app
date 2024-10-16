100151c651bf07326446c8dcf02af189c1fa1ee0
bagetz <gmbinos@gmail.com>
2024-10-17 02:41:17 +0800

    Routing Basics

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './feature/home/home.component';
+import { RecipeComponent } from './feature/recipe/recipe.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', title: 'Home', component: HomeComponent},
+    {path: 'recipe', title: 'Recipe',component: RecipeComponent},
+];
